dc9b82fc10144637ad83deeab71e671155cda953
chloeBarraza <tinsley.chloe@gmail.com>
2022-11-09 04:33:38 -0800

    added get all to server

--- server/index.js
+++ server/index.js
@@ -6,20 +6,29 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json());
 
+
+//making a new list item
 app.post('/groceries', async(req, res) => {
     try {
         const {description} = req.body;
-        const newGrocery = await pool.query('INSERT INTO grocries (description) VALUES($1)', 
+        const newGrocery = await pool.query('INSERT INTO grocries (description) VALUES($1) RETURNING *', 
         [description]);
 
-        res.json(newGrocery);
+        res.json(newGrocery.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 })
 
-
-
+//get all
+app.get('/groceries', async(req, res) => {
+    try {
+        const allGroceries = await pool.query('SELECT * FROM grocries');
+        res.json(allGroceries.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 
 
