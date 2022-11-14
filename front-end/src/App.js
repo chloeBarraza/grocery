5ba2ab785ad87bdeecb538686b0d20b67a2bc792
chloeBarraza <tinsley.chloe@gmail.com>
2022-11-14 08:36:16 -0800

    added front-components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
-
-import './App.css';
+import "./App.css";
+import ListGroceries from "./components/ListGroceries";
 
 function App() {
-  
-
+  return <>
+  </>;
 }
 
 export default App;
---
+++ front-end/src/components/ListGroceries.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ListGroceries = () => {
+    return <h1>o</h1>
+        
+    
+};
+
+export default ListGroceries;
